fdcf3c36ce5231360e3fd4eeee8a0a18fdb8d3ce
brunonico <brunonicolas10@hotmail.com>
2021-08-24 03:40:10 -0300

    template begin

---
+++ template-engine/express-jsx.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+
+function getRenderedContent(content, object) {
+
+}
+
+function expressJsx (filePath,options,callback){
+    fs.readFile(filePath, function(err,content){
+       if (err){
+            return callback(err);
+       }
+       const rendered=getRenderedContent(content, options);
+
+       return callback(null, rendered);
+    });
+}
+
+module.exports = expressJsx;
